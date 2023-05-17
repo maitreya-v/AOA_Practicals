a7d25f4b5b15f4b1fb4df301249af5daf6242c67
maitreya-v <maitreya.vaghulade@gmail.com>
2023-05-18 02:33:26 +0530

    Bellman Ford corrected

--- BellmanFord.cpp
+++ BellmanFord.cpp
@@ -1,73 +1,74 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+#define INT_MAX 9999
+const int MAX_VERTICES = 20;
 
+void BellmanFord(int distmat[MAX_VERTICES][MAX_VERTICES], int numVertices, int edges[MAX_VERTICES][2], int numEdges) {
+    int distances[MAX_VERTICES], parents[MAX_VERTICES], source, u, v;
 
-int BellmanFord(int distmat[20][20],int numVertices,int edges[20][2],int numEdges){
+    for (int i = 0; i < numVertices; i++) {
+        distances[i] = INT_MAX;
+        parents[i] = -1;
+    }
 
-        int distances[20],parents[20],source,u,v;
+    cout << "Enter the source: ";
+    cin >> source;
 
+    distances[source] = 0;
 
-        for(int i=0;i<numVertices;i++){
-            distances[i]=1000;
-            parents[i]=-1;
-        }
+    for (int i = 0; i < numVertices - 1; i++) {
+        for (int j = 0; j < numEdges; j++) {
+            u = edges[j][0];
+            v = edges[j][1];
 
-        cout<<"Enter the source"<<endl;
-        cin>>source;
-
-        for(int i=0;i<numVertices-1;i++){
-            for(int j=0;j<numEdges;j++){
-                u=edges[j][0];
-                v=edges[j][1];
-
-                if(distances[u]+distmat[u][v]<distances[v]){
-                    distances[v]=distances[u]+distmat[u][v];
-                    parents[v]=u;
-                }
+            if (distances[u] != INT_MAX && distances[u] + distmat[u][v] < distances[v]) {
+                distances[v] = distances[u] + distmat[u][v];
+                parents[v] = u;
             }
         }
-        bool hasNegativeCycle=true;
-        for(int i=0;i<numEdges;i++){
-            u=edges[i][0];
-            v=edges[i][1];
+    }
 
-            if(distances[u]+distmat[u][v]<distances[v]){
-                hasNegativeCycle=false;
-            }
+    bool hasNegativeCycle = false;
+
+    for (int i = 0; i < numEdges; i++) {
+        u = edges[i][0];
+        v = edges[i][1];
+
+        if (distances[u] != INT_MAX && distances[u] + distmat[u][v] < distances[v]) {
+            hasNegativeCycle = true;
+            break;
         }
-        if(!hasNegativeCycle) {
-            for(int i=0;i<numVertices;i++){
-                cout<<"Vertex "<<i+1<<" distance "<<distances[i]<<" parent "<<parents[i]<<endl;
-            }
+    }
+
+    if (hasNegativeCycle) {
+        cout << "Negative cycle exists" << endl;
+    } else {
+        for (int i = 0; i < numVertices; i++) {
+            cout << "Vertex " << i << " distance " << distances[i] << " parent " << parents[i] << endl;
         }
-
-        return hasNegativeCycle;
-
+    }
 }
 
-int main()
-{
-    int numVertices,numEdges=0,distmat[20][20],edges[20][2];
+int main() {
+    int numVertices, numEdges = 0, distmat[MAX_VERTICES][MAX_VERTICES], edges[MAX_VERTICES][2];
 
-    cout<<"Enter the number of vertices"<<endl;
-    cin>>numVertices;
+    cout << "Enter the number of vertices: ";
+    cin >> numVertices;
 
-    cout<<"Enter the variables of the distance matrix"<<endl;
-    for(int i=0;i<numVertices;i++){
-        for(int j=0;j<numVertices;j++){
-            cin>>distmat[i][j];
+    cout << "Enter the values of the distance matrix: " << endl;
+    for (int i = 0; i < numVertices; i++) {
+        for (int j = 0; j < numVertices; j++) {
+            cin >> distmat[i][j];
 
-        if(distmat[i][j]!=0){
-            edges[numEdges][0]=i;
-            edges[numEdges][1]=j;
-            numEdges++;
-        }   
+            if (distmat[i][j] != 0) {
+                edges[numEdges][0] = i;
+                edges[numEdges][1] = j;
+                numEdges++;
+            }
         }
     }
 
-    int flag=BellmanFord(distmat,numVertices,edges,numEdges);
-    if(flag){
-        cout<<"Negative cycle exists"<<endl;
-    }
+    BellmanFord(distmat, numVertices, edges, numEdges);
+
     return 0;
 }
--- testing.cpp
+++ testing.cpp
@@ -1,36 +1,74 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+#define INT_MAX 9999
+const int MAX_VERTICES = 20;
 
-int main()
-{
-    int n;
-    cout<<"Enter the number of vertices\n";
-    cin>>n;
-    int adj[10][10];
-    cout<<"Enter the adjacency matrix\n";
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            cin>>adj[i][j];
-        }
+void BellmanFord(int distmat[MAX_VERTICES][MAX_VERTICES], int numVertices, int edges[MAX_VERTICES][2], int numEdges) {
+    int distances[MAX_VERTICES], parents[MAX_VERTICES], source, u, v;
+
+    for (int i = 0; i < numVertices; i++) {
+        distances[i] = INT_MAX;
+        parents[i] = -1;
     }
-    int k=1;
-    int color[10];
-    for(int i=0;i<n;i++){
-        color[i]=0;
-    } 
-    for(int i=0;i<n;i++){
-        if(color[i]){
-            continue;
-        }
-        color[i]=k;
-        for(int j=0;j<n;j++){
-            if(color[j]) continue;
-            else if(!color[j] && adj[i][j]==0) {
-                color[j]=k;
+
+    cout << "Enter the source: ";
+    cin >> source;
+
+    distances[source] = 0;
+
+    for (int i = 0; i < numVertices - 1; i++) {
+        for (int j = 0; j < numEdges; j++) {
+            u = edges[j][0];
+            v = edges[j][1];
+
+            if (distances[u] != INT_MAX && distances[u] + distmat[u][v] < distances[v]) {
+                distances[v] = distances[u] + distmat[u][v];
+                parents[v] = u;
             }
         }
-        k++;
     }
-    cout<<k<<"\n";
+
+    bool hasNegativeCycle = false;
+
+    for (int i = 0; i < numEdges; i++) {
+        u = edges[i][0];
+        v = edges[i][1];
+
+        if (distances[u] != INT_MAX && distances[u] + distmat[u][v] < distances[v]) {
+            hasNegativeCycle = true;
+            break;
+        }
+    }
+
+    if (hasNegativeCycle) {
+        cout << "Negative cycle exists" << endl;
+    } else {
+        for (int i = 0; i < numVertices; i++) {
+            cout << "Vertex " << i << " distance " << distances[i] << " parent " << parents[i] << endl;
+        }
+    }
+}
+
+int main() {
+    int numVertices, numEdges = 0, distmat[MAX_VERTICES][MAX_VERTICES], edges[MAX_VERTICES][2];
+
+    cout << "Enter the number of vertices: ";
+    cin >> numVertices;
+
+    cout << "Enter the values of the distance matrix: " << endl;
+    for (int i = 0; i < numVertices; i++) {
+        for (int j = 0; j < numVertices; j++) {
+            cin >> distmat[i][j];
+
+            if (distmat[i][j] != 0) {
+                edges[numEdges][0] = i;
+                edges[numEdges][1] = j;
+                numEdges++;
+            }
+        }
+    }
+
+    BellmanFord(distmat, numVertices, edges, numEdges);
+
     return 0;
 }
